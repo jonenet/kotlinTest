aca0cac761990f29e7eeab2525db32688e108906
jonenet <jonenet0619@gmail.com>
2019-05-08 21:46:59 +0800

    Test 3 迭代器 chain

--- kotlintestlib/src/main/java/launcher/lzui/com/kotlintestlib/okhttpChain/RealInterceptorChain.kt
+++ kotlintestlib/src/main/java/launcher/lzui/com/kotlintestlib/okhttpChain/RealInterceptorChain.kt
@@ -10,6 +10,7 @@ class RealInterceptorChain(var interceptors: List<Interceptor>, var index: Int, 
         // 每调用一次都会执行intercept方法，并把下一个chain传下去，在intercept方法中，又会执行request方法和process方法，
         // 如果到了最后一个还没有结果返回，就认为结束
         // 如果有interceptor没有执行 process 方法，也会结束链
+        // 1，2
         return interceptor.intercept(next)
     }
 
