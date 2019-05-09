2ebd449b3e29a41795c480ce02d72002c54beb4c
jonenet <jonenet0619@gmail.com>
2019-05-09 18:49:15 +0800

    OKIO test1

--- kotlintestlib/src/main/java/launcher/lzui/com/kotlintestlib/okio/OkioFileUtils.kt
+++ kotlintestlib/src/main/java/launcher/lzui/com/kotlintestlib/okio/OkioFileUtils.kt
@@ -1,11 +1,7 @@
 package launcher.lzui.com.kotlintestlib.okio
 
-import okio.Sink
-import okio.buffer
-import okio.sink
-import okio.source
-import java.io.File
-import java.io.FileOutputStream
+import okio.*
+import java.io.*
 import java.nio.charset.Charset
 
 
@@ -13,7 +9,73 @@ class OkioFileUtils {
 
     companion object {
 
+//        private fun File.sink(append: Boolean = false): Sink {
+//            return FileOutputStream(this, append).sink()
+//        }
+
+        fun copyOkio(oldFile: File, copyFile: File): Boolean {
+            var flag: Boolean
+            var sourceBuffer: BufferedSource? = null
+            var sinkBuffer: BufferedSink? = null
+            try {
+                sourceBuffer = oldFile.source().buffer()
+                sinkBuffer = copyFile.sink().buffer()
+                while (true) {
+                    val readByteArray = sourceBuffer.readByteArray()
+                    if (readByteArray.isNotEmpty()) {
+                        sinkBuffer.write(readByteArray).flush()
+                    } else {
+                        break
+                    }
+                }
+                flag = true
+            } catch (e: Exception) {
+                flag = false
+                e.printStackTrace()
+            } finally {
+                sourceBuffer!!.close()
+                sinkBuffer!!.close()
+            }
+            return flag
+        }
+
+        fun copyStream(oldFile: File, copyFile: File): Boolean {
+            var flag: Boolean
+            var fileInputStream: BufferedInputStream? = null
+            var fileOutputStream: BufferedOutputStream? = null
+            try {
+                fileInputStream = BufferedInputStream(FileInputStream(oldFile))
+                fileOutputStream = BufferedOutputStream(FileOutputStream(copyFile))
+                val byteArray = ByteArray(4096)
+                while (true) {
+                    val len = fileInputStream.read(byteArray)
+                    if (len != -1) {
+                        fileOutputStream.write(byteArray, 0, len)
+                    } else {
+                        break
+                    }
+                }
+                flag = true
+            } catch (e: IOException) {
+                e.printStackTrace()
+                flag = false
+            } finally {
+                fileInputStream!!.close()
+                fileOutputStream!!.close()
+            }
+            return flag
+
+        }
+
+        fun writeFile(file: File) {
+            // Sink 写数据
+            val buffer = file.sink(true).buffer()
+            buffer.writeString("test1\ntest2\ncode\ntest3", Charset.defaultCharset()).flush()
+        }
+
+
         fun readFile(file: File): String? {
+            // Source 读数据
             val source = file.source()
             val buffer = source.buffer()
 
@@ -29,15 +91,7 @@ class OkioFileUtils {
             return null
         }
 
-        fun writeFile(file: File) {
-            val buffer = file.sink(true).buffer()
-            buffer.writeString("test1\ntest2\ncode\ntest3", Charset.defaultCharset()).flush()
-        }
 
-    }
-
-    fun File.sink(append: Boolean = false): Sink {
-       return FileOutputStream(this, append).sink()
     }
 
 
--- kotlintestlib/src/main/java/launcher/lzui/com/kotlintestlib/okio/OkioTest.kt
+++ kotlintestlib/src/main/java/launcher/lzui/com/kotlintestlib/okio/OkioTest.kt
@@ -1,10 +1,35 @@
 package launcher.lzui.com.kotlintestlib.okio
 
 import java.io.File
+import java.util.*
 
 fun main() {
-    val file = File("test.txt")
-    OkioFileUtils.writeFile(file)
+    var sourceFile1 = File("source1.exe")
+    var copyFile1 = File("copy1.exe")
 
-    OkioFileUtils.readFile(file)
+    var sourceFile2 = File("source2.exe")
+    var copyFile2 = File("copy2.exe")
+//    OkioFileUtils.writeFile(file)
+//
+//    OkioFileUtils.readFile(file)
+    Thread(Runnable {
+        println("copyOkio = ${Date(System.currentTimeMillis())}")
+        for (i in (0..200)) {
+            OkioFileUtils.copyOkio(sourceFile1, copyFile1)
+        }
+        println("copyOkio = ${Date(System.currentTimeMillis())}")
+    }).start()
+
+    println("=================================================")
+
+    Thread(Runnable {
+        println("copyStream = ${Date(System.currentTimeMillis())}")
+        for (i in (0..200)) {
+            OkioFileUtils.copyStream(sourceFile2, copyFile2)
+        }
+        println("copyStream = ${Date(System.currentTimeMillis())}")
+    }).start()
+
+//    var decodeHex = "89504e470d0a1a0a".decodeHex()
+//    println(decodeHex)
 }
